5466db2d7ffa1e07de8fb7ae58fd376a151c4bfd
Hadikhan4535 <pakistanigamer4535@gmail.com>
2024-10-18 01:01:47 -0700

    add env file and setup enviroment variable

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -24,7 +24,7 @@ export default function DataTable() {
 
   const fetchData = async () => {
     // Fetch data from your API or any other source
-    const response = await fetch("http://localhost:3005/user/all");
+    const response = await fetch(`${process.env.BACKEND_URL}/user/all`);
     const data = await response.json();
     console.log("data", data, data.length);
     setRows(data);
@@ -32,6 +32,7 @@ export default function DataTable() {
 
   useEffect(() => {
     fetchData();
+    console.log(process.env.BACKEND_URL);
   }, []);
 
   return (
