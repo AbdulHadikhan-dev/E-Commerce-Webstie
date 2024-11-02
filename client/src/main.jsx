c94f007bfaa1634f057794b84f05e2ba9575746b
Hadikhan4535 <pakistanigamer4535@gmail.com>
2024-11-02 11:11:00 -0700

    fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,7 +21,7 @@ createRoot(document.getElementById("root")).render(
     domain="dev-uj3eub3g6fo2poov.us.auth0.com"
     clientId="qkN0ZCwqn1V2mrt083FajexdFACUhIXq"
     authorizationParams={{
-      redirect_uri: window.location.pathname,
+      redirect_uri: window.location.href,
     }}
   >
     <Provider store={store}>
